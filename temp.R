fa033227a086f6bb839de7b3a243f0b96bd07bc1
David Kane <dave.kane@gmail.com>
2025-07-16 19:23:02 -0400

    ready for class

--- temp.R
+++
@@ -1,9 +0,0 @@
-# install.packages("AER")
-library(AER)
-library(tidyverse)
-help(package = "AER")
-data("ResumeNames")
-x <- as_tibble(ResumeNames) |> 
-  select(name, gender, ethnicity, quality, call, city, jobs, experience, honors, holes, special)
-
-write_csv(x, file = "../bootcamp/_internal/data/resume.csv")
